6d78bd649f39a005843985538c687cc8e2e0a74b
Gilbert Song <songzihao1990@gmail.com>
2018-05-01 13:26:47 -0700

    Added framework metric for offer operation.
    Review: https://reviews.apache.org/r/66847/

--- src/master/master.cpp
+++ src/master/master.cpp
@@ -4306,6 +4306,10 @@ void Master::_accept(
     return;
   }
 
+  foreach (const Offer::Operation& operation, accept.operations()) {
+    framework->metrics.incrementOperation(operation);
+  }
+
   Slave* slave = slaves.registered.get(slaveId);
 
   if (slave == nullptr || !slave->connected) {
