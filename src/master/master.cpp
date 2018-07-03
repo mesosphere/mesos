c4fb6b3ebc4c9e4d6ea9abf58ac0c0d0f5c51221
Gastón Kleiman <gaston.kleiman@gmail.com>
2018-07-03 12:09:16 -0700

    Improved logging for offers and inverse offers.
    Log offer IDs and inverse offer IDs when sending out offers and
    inverse offers so it is easier to match them to their ACCEPT or DECLINE
    calls and removals.
    
    Also log at `VLOG(2)` level which resources are offered.
    
    NOTE: It is possible to enable `VLOG(2)` logs just for `master.cpp` by
    setting the following env variable when starting the master:
    `GLOG_vmodule=master=2`.
    
    Review: https://reviews.apache.org/r/67817

--- src/master/master.cpp
+++ src/master/master.cpp
@@ -7536,6 +7536,9 @@ void Master::offer(
   // and a single allocation role.
   ResourceOffersMessage message;
 
+  // We keep track of the offer IDs so that we can log them.
+  vector<OfferID> offerIds;
+
   foreachkey (const string& role, resources) {
     foreachpair (const SlaveID& slaveId,
                  const Resources& offered,
@@ -7680,6 +7683,13 @@ void Master::offer(
       // Add the offer *AND* the corresponding slave's PID.
       message.add_offers()->MergeFrom(offer_);
       message.add_pids(slave->pid);
+
+      offerIds.push_back(offer_.id());
+
+      VLOG(2) << "Sending offer " << offer_.id()
+              << " containing resources " << offered
+              << " on agent " << *slave
+              << " to framework " << *framework;
     }
   }
 
@@ -7687,8 +7697,7 @@ void Master::offer(
     return;
   }
 
-  LOG(INFO) << "Sending " << message.offers().size()
-            << " offers to framework " << *framework;
+  LOG(INFO) << "Sending offers " << offerIds << " to framework " << *framework;
 
   framework->send(message);
 }
@@ -7776,8 +7785,13 @@ void Master::inverseOffer(
     return;
   }
 
-  LOG(INFO) << "Sending " << message.inverse_offers().size()
-            << " inverse offers to framework " << *framework;
+  vector<OfferID> inverseOfferIds;
+  foreach (const InverseOffer& inverseOffer, message.inverse_offers()) {
+    inverseOfferIds.push_back(inverseOffer.id());
+  }
+
+  LOG(INFO) << "Sending inverse offers " << inverseOfferIds << " to framework "
+            << *framework;
 
   framework->send(message);
 }
