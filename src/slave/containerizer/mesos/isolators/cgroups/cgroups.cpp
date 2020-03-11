d7138d12fac28ece16981e21e6ed99d9e08c1b31
Qian Zhang <zhq527725@gmail.com>
2020-03-11 12:52:59 -0700

    Updated the `update()` method of subsystem to handle resource limits.
    Review: https://reviews.apache.org/r/71885/

--- src/slave/containerizer/mesos/isolators/cgroups/cgroups.cpp
+++ src/slave/containerizer/mesos/isolators/cgroups/cgroups.cpp
@@ -863,7 +863,8 @@ Future<Nothing> CgroupsIsolatorProcess::update(
       updates.push_back(subsystem->update(
           containerId,
           infos[containerId]->cgroup,
-          resourceRequests));
+          resourceRequests,
+          resourceLimits));
     }
   }
 
